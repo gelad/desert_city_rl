b5583b360cb04d40b4b8d4548dee293c61b75275
gelad <gelad655@mail.ru>
2016-08-30 22:54:29 +0300

    Moved FOV recompute to action functons.

--- fov_los.py
+++ fov_los.py
@@ -6,4 +6,4 @@ import tdl
 
 def get_fov(x, y, loc, radius):
     """ Function that calculates FOV. Now just a wrapper around tdl function """
-    return tdl.map.quick_fov(x, y, loc.is_cell_transparent, 'BASIC', radius)
+    return tdl.map.quick_fov(x, y, loc.is_cell_transparent, 'SHADOW', radius)
