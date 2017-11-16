73dc6fbcdf9cbdf54614496332a4c1687740c254
gelad <gelad655@mail.ru>
2017-11-16 21:54:33 +0200

    Removed old junk code.

--- dc_rl.py
+++ dc_rl.py
@@ -1,29 +1,7 @@
 import game_view
 import dataset
 
-import jsonpickle
-
-
-def player_death():
-    """ Function that is called if player is dead """
-    # show window with death recap, that returns to main menu
-    chosen = ui.show_menu_text_above(win_mgr=game_view.win_mgr, caption='You died!',
-                                     options=['Return to main menu', 'Exit'], keys=None,
-                                     texts='Horrors of the Desert City got you.',
-                                     prev_window=game_view.win_mgr.active_window,
-                                     width=40,
-                                     text_height=2)
-    if chosen[0] == 'Return to main menu':
-        to_menu = True
-    else:
-        to_menu = False
-    return to_menu
-
 # HERE PROGRAM RUN STARTS
 dataset.initialize()
-settings_file = open('data/settings.json')  # open settings file
-settings = jsonpickle.loads(settings_file.read())  # load settings
-settings_file.close()
 loop = game_view.GameLoop()
 loop.run()
-
