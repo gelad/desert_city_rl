ee062661fa157a1cdca97bdcb27b7d87c9d4fbee
gelad <gelad655@mail.ru>
2017-11-24 23:50:28 +0200

    Removed debug check.

--- game_logic.py
+++ game_logic.py
@@ -1821,9 +1821,6 @@ class Location:
             return self.cells[dest_x][dest_y].get_move_cost()
         actor = self.cells[dest_x][dest_y].is_there_a(Actor)
         if actor:  # if there is an actor - add 'not moved' turns count to prevent monster jams in narrow places
-            if actor.not_moved > 100:
-                # TODO: actually DEBUG
-                print('not moved = ' + str(actor.not_moved))
             return self.cells[dest_x][dest_y].get_move_cost() + actor.not_moved * actor.speed
         else:
             return self.path_map[dest_x][dest_y]
